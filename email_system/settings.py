a2cce3d98ff758c170ef4c6bcd436070dd5657fe
Chandan <chandanjume@gmail.com>
2023-01-08 03:23:37 -0800

    replaced email

--- email_system/settings.py
+++ email_system/settings.py
@@ -130,8 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'itmbssc@gmail.com'
-EMAIL_HOST_PASSWORD = 'oshochandu'
+EMAIL_HOST_USER = 'chandanjume@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
